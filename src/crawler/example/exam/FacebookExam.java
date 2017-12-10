3daa88800ded18088cc39fc8d41efac3e60483e1
JimmyGimmi <lessonxii@gmail.com>
2017-12-10 09:42:59 +0800

    Facebook Exam-PCHome

--- src/crawler/example/exam/FacebookExam.java
+++ src/crawler/example/exam/FacebookExam.java
@@ -25,8 +25,8 @@ public class FacebookExam {
 
 		String uri = 
 				"https://graph.facebook.com/v2.6"
-				+ "/search?q="
-				+ "&access_token=";
+				+ "/pchome24h/posts?fields=id,link,message,created_time,likes.limit(0).summary(total_count),reactions.limit(0).summary(total_count)"
+				+ "&access_token=EAACEdEose0cBAHzhjY4H0Ba56bWji5eBSrmlQiwe1s03J16fmgARgOXE7HziOxWqV12WkvWhiZAqwVYWECfbPwjm4W9TvzsC5AiG1C0Cej7Mn68uOWCb8ntuPc1lE6FjtaBFu3ZCmgSZCWc0Nn7eyZC3ud0Vnkd61nNUBON0vnXD3yNnZCeKRWEjMFPjiQPdtEklk3VwoDAZDZD";
 
 
 		Elements elems =
@@ -41,7 +41,7 @@ public class FacebookExam {
 			String id = data.select("id").text();
 
 			// FIXIT
-			String reactions = "";
+			String reactions = data.select("reactions summary total_count").text();
 
 
 			output += id + "," + reactions + "\n";
